3091812ffa6d6a7dd00383452277727e4f06d802
Ralph Schaer <ralphschaer@gmail.com>
2026-03-07 19:02:36 +0100

    remove spring boot 3 example
    focus on the latest versions

--- springboot3/src/main/java/org/acme/dto/StoreFruitPriceDTO.java
+++
@@ -1,9 +0,0 @@
-package org.acme.dto;
-
-public record StoreFruitPriceDTO(StoreDTO store, float price) {
-  public StoreFruitPriceDTO {
-    if (price < 0) {
-      throw new IllegalArgumentException("Price must be >= 0");
-    }
-  }
-}
